f9d0f90a84f750f8b7d3861a89bc56e9363a467c
horlaide22 <hartoke2sekinat@gmail.com>
2023-02-25 13:46:39 +0100

    solution4

--- Assignment3.js
+++ Assignment3.js
@@ -35,6 +35,17 @@ function discount( costOfPurchased){
 console.log(discount(800))
 console.log(discount(1200))
 
+function profitableGamble(prob, prize, pay){
+    if ((prob * prize) > pay){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(profitableGamble(0.2, 50, 9))
+console.log(profitableGamble(0.9, 1, 2))
+console.log(profitableGamble(0,9, 3, 2))
+
 
 
 
